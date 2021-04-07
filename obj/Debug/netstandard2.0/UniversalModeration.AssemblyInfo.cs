8a35185c5c9bcae8f9dfa6bd2a458ce8ad7f8715
Feli <felinegri5@gmail.com>
2021-04-07 16:38:00 -0300

    Add KickCommand

--- obj/Debug/netstandard2.0/UniversalModeration.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/UniversalModeration.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniversalModeration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.2.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.2")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniversalModeration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniversalModeration")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.2.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
